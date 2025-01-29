4d9c6328a0d5c360db14ab916ed437112881e51f
sumo-07 <sumo.cs07@gmail.com>
2025-01-29 19:15:29 +0530

    Solution to Input and Print a Double

---
+++ Input and Print a Double@@6788d5012db25efec946f942/code.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+char* welcome() {
+    return "Welcome to Code Radar!";
+}
+
+int main() {
+    double a;
+    scanf("%d",&a);
+    printf("You entered %.4d", a);
+    return 0;
+}
